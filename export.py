63a6fa3a2f25156387e5677ca000587465e58929
Broksy <konstantin.tzantchev@gmail.com>
2020-02-19 15:51:24 +0100

    Fixed file format problem

--- export.py
+++ export.py
@@ -2,7 +2,7 @@ import basic_functions
 from PIL import Image
 
 # Supported image file formats
-file_formats = [".png", ".jpg"]
+file_formats = (".png", ".jpg")
 
 # Export image
 def export(img):
@@ -12,20 +12,10 @@ def export(img):
     if save_decision == "Y":
         name = input("Enter file name for export: ")
 
-        for i in file_formats:
-            if name.lower().endswith(i):
-                img.save(name)
-                break
-            else:
-                user_format = input("Enter export format: ").lower()
-                if user_format in file_formats:
-                    if user_format == ".jpg":
-                        img = img.convert("RGB")
-                        img.save(name + user_format)
-                    elif user_format == ".png":
-                        img = img.cenvert("RGBA")
-                        img.save(name + user_format)
-                    break
+        if name.lower().endswith(file_formats):
+            save_with_format(name, img)
+        else:
+            save_without_format(name, img)
 
     elif save_decision == "N":
         exit()
@@ -33,5 +23,24 @@ def export(img):
         print("Answer with Y or N")
         export(img)
 
-def convert_channels(format, img):
+# Save if user does not provide a format with the name
+def save_without_format(name, img):
 
+    export_format = input("Enter export format: ").lower()
+
+    if export_format == ".jpg":
+        img = img.convert("RGB")
+    elif export_format == ".png":
+        img = img.convert("RGBA")
+    img.save(name + export_format)
+
+# Save if user provides a format with the name
+def save_with_format(name, img):
+
+    name = name.lower()
+    if name.endswith(".jpg"):
+        img = img.convert("RGB")
+        img.save(name)
+    elif name.endswith(".png"):
+        img = img.convert("RGBA")
+        img.save(name)
