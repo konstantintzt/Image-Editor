c68dd80e4a191475032a9ea870e838afc98ae0ca
Broksy <konstantin.tzantchev@gmail.com>
2020-03-29 00:23:47 +0100

    Add text to images and color picker

--- basic_functions.py
+++ basic_functions.py
@@ -1,5 +1,4 @@
-from PIL import ImageFilter
-from PIL import Image
+from PIL import ImageFilter, Image, ImageDraw, ImageFont
 # Basic features of the editor
 
 def grayscale(im):
@@ -32,17 +31,39 @@ def invertColors(im):
     return im
             
 
-def colorToTransparency(im):
+def colorToTransparency(im, color):
 
     im = im.convert("RGBA")
     
-    color_to_change = tuple(map(int,input("Enter color to be changed to transparency: [R,G,B] ").split(",")))
-    print("Color to change is:", color_to_change)
+    #color_to_change = tuple(map(int,input("Enter color to be changed to transparency: [R,G,B] ").split(",")))
+    #print("Color to change is:", color_to_change)
 
     for x in range(im.size[0]):
         for y in range(im.size[1]):
             current_pixel = im.getpixel((x, y))
-            if current_pixel[0] == color_to_change[0] and current_pixel[1] == color_to_change[1] and current_pixel[2] == color_to_change[2]:
+            if current_pixel[0] == color[0] and current_pixel[1] == color[1] and current_pixel[2] == color[2]:
                 im.putpixel((x,y), (0,0,0,0))
 
     return im
+
+def draw_point(im, x, y, color):
+
+    for x_coord in range(x-3, x+3):
+        for y_coord in range(y-3, y+3):
+            im.putpixel((x_coord,y_coord), color)
+    
+    return im
+
+def add_text(im, x, y, content, color):
+    im = im.convert("RGBA")
+    d = ImageDraw.Draw(im)
+    font = ImageFont.truetype("comic.ttf", 36)
+
+    d.text((x,y), content, fill=color, font=font)
+
+    return im
+
+def pick_color(im, x, y):
+
+    color = im.getpixel((x,y))
+    return color
--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageTk
-from tkinter import Tk, Label, Button, Canvas, Toplevel
+from tkinter import Tk, Label, Button, Canvas, Toplevel, simpledialog
 from tkinter.filedialog import askopenfilename
 import basic_functions
 from export import export
@@ -8,6 +8,7 @@ img = None
 app_title = "Broksy Image Editor v0.1"
 root = None
 tk_im = None
+color = (255,255,255)
 
 # Tkinter dialog to chose file
 def get_image():
@@ -22,8 +23,52 @@ def display_image(im, canvas):
     global tk_im
     global image_window
     canvas.pack(side="top",fill="both",expand="yes")
-    canvas.create_image(im.size[0]/2+10, im.size[1]/2+10, image=tk_im)
+    canvas.create_image(im.size[0]/2, im.size[1]/2, image=tk_im)
 
+def draw_mode():
+    global canvas
+    canvas.bind("<Button 1>", draw_point)
+
+def text_mode():
+    global canvas
+    canvas.bind("<Button 1>", add_text)
+
+def color_picker():
+    global canvas
+    canvas.bind("<Button 1>", pick_color)
+
+def draw_point(event):
+    global tk_im
+    global img
+    global canvas
+    global color
+    img = basic_functions.draw_point(img, event.x, event.y, color)
+    tk_im = ImageTk.PhotoImage(img)
+    canvas.delete("all")
+    display_image(img, canvas)
+
+def add_text(event):
+    global tk_im
+    global img
+    global canvas
+    global color
+    content = simpledialog.askstring(title=app_title, prompt="Enter text: ")
+    try:
+        img = basic_functions.add_text(img, event.x, event.y, content,color)
+    except TypeError:
+        pass
+    tk_im = ImageTk.PhotoImage(img)
+    canvas.delete("all")
+    display_image(img, canvas)
+
+def pick_color(event):
+    global tk_im
+    global img 
+    global canvas
+    global color
+
+    color = basic_functions.pick_color(img, event.x, event.y)
+    print(color)
 
 def grayscale():
 
@@ -67,14 +112,15 @@ def color_to_transparency():
     global tk_im
     global img
     global canvas
-    img = basic_functions.colorToTransparency(img)
+    global color
+    img = basic_functions.colorToTransparency(img, color)
     tk_im = ImageTk.PhotoImage(img)
     canvas.delete("all")
     display_image(img, canvas)
 
 
 root = Tk()
-root.geometry("200x200")
+root.geometry("400x400")
 root.title(app_title)
 
 button = Button(master=root, command=grayscale, text="Grayscale")
@@ -89,13 +135,19 @@ button = Button(master=root, command=color_to_transparency, text="Color to trans
 button.pack()
 button = Button(master=root, command= lambda: export(img), text="Save")
 button.pack()
+button = Button(master=root, command=draw_mode, text="Draw")
+button.pack()
+button = Button(master=root, command=text_mode, text="Text")
+button.pack()
+button = Button(master=root, command=color_picker, text="Color picker")
+button.pack()
 
 # Variables for PIL image and image for Tkinter canvas
 img,tk_im = get_image()
 
 image_window = Toplevel()
 image_window.title(app_title)
-image_window.geometry(str(img.size[0]+20)+"x"+str(img.size[1]+20))
+image_window.geometry(str(img.size[0])+"x"+str(img.size[1]))
 canvas = Canvas(master=image_window)
 display_image(img, canvas)
 
